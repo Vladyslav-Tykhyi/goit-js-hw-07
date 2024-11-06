d1076d478561bb4b0c759c0e5c4a2af61ad4a41d
Vladyslav Tykhyi <vladleonidovich01@gmail.com>
2024-11-06 18:41:13 +0100

    first-try

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+const colorSpan = document.querySelector(".color");
+const changeColorBtn = document.querySelector(".change-color");
+
+// Додаємо обробник події на кнопку
+changeColorBtn.addEventListener("click", () => {
+  const randomColor = getRandomHexColor(); // Генеруємо випадковий колір
+  body.style.backgroundColor = randomColor; // Змінюємо фон body
+  colorSpan.textContent = randomColor; // Відображаємо колір у span
+});
